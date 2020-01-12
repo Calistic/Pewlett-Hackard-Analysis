a0340fe499e6208f1ed5302b902e66b7ef700921
Calistic <mandn23@gmail.com>
2020-01-12 12:47:33 -0800

    initial commit

---
+++ Queries/queries.sql
@@ -0,0 +1,98 @@
+-- DROP TABLE num_title;
+
+-- PART 1a -- PART 1a -- PART 1a -- PART 1a -- PART 1a --
+-- Number of [titles] Retiring
+-- -- Create list of retirement age employees
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	ti.title,
+	s.salary,
+	s.from_date
+INTO retire_list
+FROM retirement_info AS ri
+INNER JOIN titles AS ti
+ON (ri.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ti.emp_no = s.emp_no);
+
+-- PART 1b -- PART 1b -- PART 1b -- PART 1b -- PART 1b --
+-- -- Exclude the rows of data containing duplicate names. 
+-- -- Only keep the most recent titles.
+SELECT retire_list.emp_no, 
+	retire_list.first_name, 
+	retire_list.last_name, 
+	retire_list.from_date, 
+	retire_list.title
+INTO retire_list_filtered
+FROM
+  (SELECT rl.emp_no, 
+   		rl.first_name, 
+   		rl.last_name, 
+   		rl.from_date, 
+   		rl.title,
+     ROW_NUMBER() OVER 
+	(PARTITION BY (rl.first_name, rl.last_name) 
+	 ORDER BY rl.from_date DESC) rn
+  FROM retire_list AS rl
+  ) retire_list
+WHERE rn = 1;
+
+-- List the frequency count of employee titles
+SELECT
+  rlf.title,
+  count(*)
+INTO retire_list_count
+FROM retire_list_filtered AS rlf
+GROUP BY
+  title;
+
+-- PART 1c -- PART 1c -- PART 1c -- PART 1c -- PART 1c --
+-- -- Get list of everyone with required information
+CREATE VIEW mentor_long_list AS
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	ti.title,
+	s.salary,
+	s.from_date
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ti.emp_no = s.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+-- -- Exclude the rows of data containing duplicate names.
+CREATE VIEW mentor_short_list AS
+SELECT mentor_long_list.emp_no, 
+	mentor_long_list.first_name, 
+	mentor_long_list.last_name, 
+	mentor_long_list.from_date, 
+	mentor_long_list.title
+FROM
+  (SELECT mll.emp_no, 
+   		mll.first_name, 
+   		mll.last_name, 
+   		mll.from_date, 
+   		mll.title,
+     ROW_NUMBER() OVER 
+	(PARTITION BY (mll.first_name, mll.last_name) 
+	 ORDER BY mll.from_date DESC) rn
+  FROM mentor_long_list AS mll
+  ) mentor_long_list
+WHERE rn = 1;
+
+-- -- Filter for birth date and currently employed
+CREATE TABLE mentor_list AS
+SELECT msl.emp_no,
+	msl.first_name,
+	msl.last_name,
+	msl.title,
+	msl.from_date,
+	de.to_date
+FROM mentor_short_list AS msl
+INNER JOIN dept_emp AS de
+ON (msl.emp_no = de.emp_no)
+WHERE (to_date = '9999-01-01');
---
+++ Queries/~Archive_queries.sql
@@ -0,0 +1,246 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table to save retirment info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+SELECT * FROM retirement_info;
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+
+
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+		
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- PART 1
+-- Number of [titles] Retiring
+SELECT re.emp_no,
+	re.first_name,
+	re.last_name,
+	ti.title,
+	s.salary,
+	s.from_date
+INTO num_retire
+FROM retirement_info AS re
+INNER JOIN titles AS ti
+ON (re.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ti.emp_no = s.emp_no);
+
+-- Only the Most Recent Titles
+
+-- Exclude the rows of data containing duplicate names.
+SELECT num_retire.emp_no, 
+	num_retire.first_name, 
+	num_retire.last_name, 
+	num_retire.from_date, 
+	num_retire.title
+INTO recent_titles
+FROM
+  (SELECT nr.emp_no, 
+   		nr.first_name, 
+   		nr.last_name, 
+   		nr.from_date, 
+   		nr.title,
+     ROW_NUMBER() OVER 
+	(PARTITION BY (nr.first_name, nr.last_name) 
+	 ORDER BY nr.from_date DESC) rn
+  FROM num_retire AS nr
+  ) num_retire
+WHERE rn = 1;
+
+-- In descending order (by date), list the frequency count of employee titles
+SELECT
+  rt.title,
+--   rt.from_date,
+  count(*)
+FROM recent_titles AS rt
+GROUP BY
+  title;
+-- HAVING count(*) > 1;
+
+-- Who’s Ready for a Mentor?
+
+-----
+-- PART 1
+-- Number of [titles] Retiring
+SELECT re.emp_no,
+	re.first_name,
+	re.last_name,
+	ti.title,
+	s.salary,
+	s.from_date
+INTO num_retire
+FROM retirement_info AS re
+INNER JOIN titles AS ti
+ON (re.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ti.emp_no = s.emp_no);
+
+-- Only the Most Recent Titles
+-- -- Exclude the rows of data containing duplicate names.
+SELECT num_retire.emp_no, 
+	num_retire.first_name, 
+	num_retire.last_name, 
+	num_retire.from_date, 
+	num_retire.title
+-- INTO recent_titles
+FROM
+  (SELECT nr.emp_no, 
+   		nr.first_name, 
+   		nr.last_name, 
+   		nr.from_date, 
+   		nr.title,
+     ROW_NUMBER() OVER 
+	(PARTITION BY (nr.first_name, nr.last_name) 
+	 ORDER BY nr.from_date DESC) rn
+  FROM num_retire AS nr
+  ) num_retire
+WHERE rn = 1;
+
+-- In descending order (by date), list the frequency count of employee titles
+SELECT
+  rt.title,
+  count(*)
+INTO num_title
+FROM recent_titles AS rt
+GROUP BY
+  title;
+
+-- Who’s Ready for a Mentor?
+SELECT rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	rt.from_date,
+	de.to_date,
+	e.birth_date
+-- INTO temp
+FROM recent_titles AS rt
+INNER JOIN dept_emp AS de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN employees AS e
+ON (de.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1955-01-01' AND '1965-12-31');
